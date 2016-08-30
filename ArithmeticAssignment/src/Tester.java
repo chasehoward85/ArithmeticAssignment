61355b1a40e6c2b8a7658fb17e93fbc0a6255f0a
Chase Howard <chasehoward85@gmail.com>
2016-08-30 12:03:55 -0600

    Arithmetic Assignment

---
+++ ArithmeticAssignment/src/Tester.java
@@ -0,0 +1,13 @@
+public class Tester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 79 + 3 * (4 + 82 - 68) - 7 + 19;
+		int y = (179 + 21 + 10) / 7 + 181;
+		int z = 10389 * 56 * 11 + 2246;
+		System.out.println("79 + 3 * (4 + 82 - 68) - 7 + 19 = " + x);
+		System.out.println("(179 + 21 + 10) / 7 + 181 = " +  y);
+		System.out.println("10389 * 56 * 11 + 2246 = " + z);
+	}
+
+}
